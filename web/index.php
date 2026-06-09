c4905c2534ae7340a932ab6e972226bd334921ec
zinh8803 <ngoquocvinh2003@gmail.com>
2026-06-09 11:02:35 +0700

    chore: configure YII_DEBUG and YII_ENV from .env

--- web/index.php
+++ web/index.php
@@ -2,15 +2,16 @@
 
 declare(strict_types=1);
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-
+// comment out the following two lines when deployed to production
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
+
+defined('YII_DEBUG') or define('YII_DEBUG', filter_var($_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOLEAN));
+defined('YII_ENV') or define('YII_ENV', $_ENV['APP_ENV'] ?? 'prod');
+
+
 
 $config = require __DIR__ . '/../config/web.php';
 
