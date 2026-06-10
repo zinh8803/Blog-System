507a8a7d5e7bbc715c050bcff6df38ed570b9a6e
zinh8803 <ngoquocvinh2003@gmail.com>
2026-06-10 16:11:10 +0700

    feat(rbac): initialize roles permissions and post owner rule

--- commands/RbacController.php
+++ commands/RbacController.php
@@ -3,6 +3,7 @@
 namespace app\commands;
 
 use app\models\User;
+use app\rbac\PostOwnerRule;
 use Yii;
 use yii\console\Controller;
 
@@ -25,6 +26,8 @@ class RbacController extends Controller
             'post.create',
             'post.update',
             'post.delete',
+            'post.updateOwn',
+            'post.deleteOwn',
             'post.updateStatus',
 
             'tag.index',
@@ -44,11 +47,7 @@ class RbacController extends Controller
             'comment.update',
             'comment.delete',
 
-            'like.index',
-            'like.view',
-            'like.create',
-            'like.update',
-            'like.delete',
+            'like.toggle',
 
             'file.index',
             'file.view',
@@ -62,130 +61,97 @@ class RbacController extends Controller
             $auth->add($permission);
         }
 
-        $user = $auth->createRole('reader');
-        $auth->add($user);
+        $rule = new PostOwnerRule();
+        $auth->add($rule);
 
-        $editor = $auth->createRole('author');
-        $auth->add($editor);
+        $postUpdateOwn = $auth->getPermission('post.updateOwn');
+        $postUpdateOwn->ruleName = $rule->name;
+        $auth->update('post.updateOwn', $postUpdateOwn);
+
+        $postDeleteOwn = $auth->getPermission('post.deleteOwn');
+        $postDeleteOwn->ruleName = $rule->name;
+        $auth->update('post.deleteOwn', $postDeleteOwn);
+
+        $auth->addChild($postUpdateOwn, $auth->getPermission('post.update'));
+        $auth->addChild($postDeleteOwn, $auth->getPermission('post.delete'));
+
+        $reader = $auth->createRole('reader');
+        $auth->add($reader);
+
+        $author = $auth->createRole('author');
+        $auth->add($author);
 
         $admin = $auth->createRole('admin');
         $auth->add($admin);
 
-        // permissions for reader role
         foreach ([
-                     'post.index',
-                     'post.view',
-                     'tag.index',
-                     'tag.view',
-
-                     'category.index',
-                     'comment.index',
-                     'comment.view',
-                     'comment.create',
-                     'comment.update',
-                     'comment.delete',
-
-                     'like.index',
-                     'like.view',
-                     'like.create',
-                     'like.update',
-                     'like.delete',
-
-                 ] as $permissionName) {
-            $auth->addChild($user, $auth->getPermission($permissionName));
+            'comment.create',
+            'like.toggle',
+        ] as $permissionName) {
+            $auth->addChild($reader, $auth->getPermission($permissionName));
         }
 
-        // permissions for author role
         foreach ([
-                     'post.index',
-                     'post.view',
-                     'post.create',
-                     'post.update',
-                     'comment.index',
-                     'comment.view',
-                     'comment.create',
-                     'comment.update',
-                     'comment.delete',
-                     'like.index',
-                     'like.view',
-                     'like.create',
-                     'like.update',
-                     'like.delete',
-                 ] as $permissionName) {
-            $auth->addChild($editor, $auth->getPermission($permissionName));
+            'post.create',
+            'post.updateOwn',
+            'post.deleteOwn',
+            'comment.create',
+            'like.toggle',
+        ] as $permissionName) {
+            $auth->addChild($author, $auth->getPermission($permissionName));
         }
 
-        // permissions for admin role
+        $auth->addChild($author, $reader);
+
         foreach ($permissions as $permissionName) {
-            $auth->addChild($admin, $auth->getPermission($permissionName));
+            $permission = $auth->getPermission($permissionName);
+
+            if ($permission) {
+                $auth->addChild($admin, $permission);
+            }
         }
 
+
+        $this->actionCreateUser('admin', 'admin@gmail.com', 'admin');
+        $this->actionCreateUser('admin1', 'admin1@gmail.com', 'admin');
+        $this->actionCreateUser('reader', 'reader@gmail.com', 'reader');
+        $this->actionCreateUser('reader1', 'reader1@gmail.com', 'reader');
+        $this->actionCreateUser('reader2', 'reader2@gmail.com', 'reader');
+        $this->actionCreateUser('author', 'author@gmail.com', 'author');
+        $this->actionCreateUser('author1', 'author1@gmail.com', 'author');
+        $this->actionCreateUser('author2', 'author2@gmail.com', 'author');
         echo "RBAC init success\n";
     }
 
-    public function actionCreateAdmin()
+    public function actionCreateUser($username, $email, $role)
     {
-        $user = User::findOne(['email' => 'admin@gmail.com']);
+        $auth = Yii::$app->authManager;
+
+        $rbacRole = $auth->getRole($role);
+        if (!$rbacRole) {
+            echo "Role {$role} does not exist.\n";
+            return;
+        }
+
+        $user = User::findOne(['email' => $email]);
 
         if (!$user) {
             $user = new User();
-            $user->username = 'admin';
-            $user->email = 'admin@gmail.com';
+            $user->username = $username;
+            $user->email = $email;
             $user->setPassword('123456');
             $user->generateAccessToken();
             $user->generateAuthKey();
-            // $user->created_at = date('Y-m-d H:i:s');
-            // $user->updated_at = date('Y-m-d H:i:s');
-            $user->save(false);
+
+            if (!$user->save(false)) {
+                echo "Create user failed.\n";
+                return;
+            }
         }
 
-        Yii::$app->authManager->assign(
-            Yii::$app->authManager->getRole('admin'),
-            $user->id
-        );
+        $auth->revokeAll($user->id);
+        $auth->assign($rbacRole, $user->id);
 
-        echo "Admin created successfully.\n";
-    }
-
-    public function actionCreateReader()
-    {
-        $user = User::findOne(['email' => 'reader@gmail.com']);
-        if (!$user) {
-            $user = new User();
-            $user->username = 'reader';
-            $user->email = 'reader@gmail.com';
-            $user->setPassword('123456');
-            $user->generateAccessToken();
-            $user->generateAuthKey();
-            $user->save(false);
-        }
-
-        Yii::$app->authManager->assign(
-            Yii::$app->authManager->getRole('reader'),
-            $user->id
-        );
-
-        echo "Reader created successfully.\n";
-    }
-
-    public function actionCreateAuthor()
-    {
-        $user = User::findOne(['email' => 'author@gmail.com']);
-        if (!$user) {
-            $user = new User();
-            $user->username = 'author';
-            $user->email = 'author@gmail.com';
-            $user->setPassword('123456');
-            $user->generateAccessToken();
-            $user->generateAuthKey();
-            $user->save(false);
-        }
-
-        Yii::$app->authManager->assign(
-            Yii::$app->authManager->getRole('author'),
-            $user->id
-        );
-
-        echo "Author created successfully.\n";
+        echo "User {$email} assigned role {$role} successfully.\n";
     }
 }
