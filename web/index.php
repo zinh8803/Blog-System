9270cd0f912629f3fafd0328877a4a363cbc86bb
zinh8803 <ngoquocvinh2003@gmail.com>
2026-06-11 17:17:55 +0700

    chore: setup yii2 debug toolbar

--- web/index.php
+++ web/index.php
@@ -3,15 +3,14 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-// comment out the following two lines when deployed to production
+
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 defined('YII_DEBUG') or define('YII_DEBUG', filter_var($_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOLEAN));
 defined('YII_ENV') or define('YII_ENV', $_ENV['APP_ENV'] ?? 'prod');
 
-
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
