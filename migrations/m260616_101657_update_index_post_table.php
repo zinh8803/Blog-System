db9b1ea6b549cfd8a24a41f778dd784e8dce5294
zinh8803 <ngoquocvinh2003@gmail.com>
2026-06-16 17:28:07 +0700

    perf(post): add index status is_deleted id

---
+++ migrations/m260616_101657_update_index_post_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\db\Migration;
+
+class m260616_101657_update_index_post_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createIndex('idx_posts_status_deleted_id', '{{%posts}}', ['status', 'is_deleted', 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropIndex('idx_post_status_is_deleted', '{{%posts}}');
+    }
+
+
+}
