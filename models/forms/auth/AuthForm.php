18c124ccf8c06516e1ad4b770a90bffcc01125a8
zinh8803 <ngoquocvinh2003@gmail.com>
2026-06-26 15:49:39 +0700

    feat(auth): add email OTP validation for user registration

--- models/forms/auth/AuthForm.php
+++ models/forms/auth/AuthForm.php
@@ -2,6 +2,7 @@
 
 namespace app\models\forms\auth;
 
+use app\models\OtpMail;
 use app\models\User;
 use RuntimeException;
 use Yii;
@@ -11,11 +12,12 @@ class AuthForm extends User
     const SCENARIO_REGISTER = 'create';
     const SCENARIO_LOGIN = 'login';
     public $password;
+    public $otp;
 
     public function scenarios()
     {
         return [
-            self::SCENARIO_REGISTER => ['username', 'email', 'password'],
+            self::SCENARIO_REGISTER => ['username', 'email', 'password', 'otp'],
             self::SCENARIO_LOGIN => ['email', 'password'],
         ];
     }
@@ -23,11 +25,25 @@ class AuthForm extends User
     public function rules(): array
     {
         return [
-            [['username', 'email', 'password'], 'required'],
+            [['username', 'email', 'password', 'otp'], 'required'],
             ['email', 'email'],
+            ['otp', 'validateOtp'],
             ['email', 'unique', 'targetClass' => User::class, 'on' => self::SCENARIO_REGISTER],
             ['username', 'unique', 'targetClass' => User::class, 'on' => self::SCENARIO_REGISTER],
         ];
+    }
+
+    public function validateOtp($attribute, $params)
+    {
+        $otpMail = OtpMail::find()->where(['email' => $this->email, 'otp' => $this->otp])->one();
+        if (!$otpMail) {
+            $this->addError($attribute, 'Invalid OTP');
+            return;
+        }
+
+        if ($otpMail->isExpired()) {
+            $this->addError($attribute, 'OTP has expired');
+        }
     }
 
     public function registerUser(): ?User
@@ -44,7 +60,7 @@ class AuthForm extends User
         if (!$user->save(false)) {
             throw new RuntimeException(Yii::t('app', 'Register failed'));
         }
-
+        OtpMail::deleteAll(['email' => $this->email]);
         $role = Yii::$app->authManager->getRole('reader');
 
         if ($role) {
